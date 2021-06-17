c78b8480a5ff87c673c30e4451d933e7af03cd08
Neko <conan.neko.lin@gmail.com>
2021-06-17 13:56:12 +0800

    Finishied Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 //import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 //define my component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm Neko.</p>
+      <StaticImage
+        alt="img fromwallhaven"
+        src="../images/neko.jpg"
+        />
     </Layout>
   )
 }
