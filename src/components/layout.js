f9ff82124b603ae27aba19d4036f7e9ce7dd8d81
Neko <conan.neko.lin@gmail.com>
2021-06-18 11:26:13 +0800

    Finished Gatsby Tutorail Part 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,48 @@
 //import
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { container,heading,navLinks,navLinkItem,navLinkText } from './layout.module.css'
+import { Link, useStaticQuery,graphql } from 'gatsby'
+import { 
+    container,
+    siteTitle,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText 
+} from './layout.module.css'
 
 //layout
-const Layout = ({pageTitle, children}) => {
+const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                 }
+            }
+        }  
+    `)
+
     return (
         <main className={container}>
-            <title>{pageTitle}</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <p className={siteTitle}>{data.site.siteMetadata.title}</p>
             <nav>
                 <ul className={navLinks}>
-                    <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-                    <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}>
+                        <Link to="/" className={navLinkText}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>
+                            Blog
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/about" className={navLinkText}>
+                            About
+                        </Link>
+                    </li>
                 </ul>
             </nav>
             <h1 className={heading}>{pageTitle}</h1>
