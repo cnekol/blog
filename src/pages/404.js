9d98361a0ed85b162e919aec67ffcfe45e3d74d2
Neko <conan.neko.lin@gmail.com>
2021-07-01 01:01:33 +0800

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,8 +10,9 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
+      <span class="material-icons">back_hand</span>
       <h1>404: Not Found</h1>
-      <p>你来到了世界的尽头，别人会说你好奇过头</p>
+      <p>你来到了世界尽头，别人会说你好奇过头</p>
       <p>我不这样认为</p>
       {/* <p>You just hit a route that doesn&#39;t exist... the sadness.</p> */}
     </Layout>
