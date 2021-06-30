331eeaaa89901d97d2afa83f9923fce7447fb3b6
Neko <conan.neko.lin@gmail.com>
2021-06-30 20:04:00 +0800

    add pagination to blog-list

--- gatsby-node.js
+++ gatsby-node.js
@@ -56,6 +56,22 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       })
     })
   }
+  // Create blog post list pages
+  const postsPerPage = 4
+  const numPages = Math.ceil(posts.length / postsPerPage)
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/` : `/${i + 1}`,
+      component: path.resolve("./src/templates/blog-list.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    })
+  })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
