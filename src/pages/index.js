6bc1a9403b371bb9ea6801997ced7d99cf812dcd
Neko <conan.neko.lin@gmail.com>
2021-07-01 00:45:18 +0800

    update landingPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,16 @@ import { Link, graphql } from "gatsby"
 
 const LandingPage = ({ data }) => {
   return (
-    <div className="landing-page-wrapper">
-      <Link to="/blog">
-        <h1>{data.site.siteMetadata.title}</h1>
-      </Link>
-      <p>Page Building...</p>
-    </div>
+    <body className="landing-page-wrapper">
+      <div className="landing-page-title">
+        <Link to="/blog">
+          <span class="material-icons">catching_pokemon</span>
+        </Link>
+        <h1>
+          <Link to="/blog">{data.site.siteMetadata.title}</Link>
+        </h1>
+      </div>
+    </body>
   )
 }
 
