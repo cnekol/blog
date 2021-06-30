33e0458e825a7a44be689e688d75d496d3ebe596
Neko <conan.neko.lin@gmail.com>
2021-07-01 02:04:15 +0800

    Revert "update PWA start_url to `/blog/`"
    This reverts commit 3359b0e64c76e2c2aea2ff295a54bb356c252e5d.

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
       options: {
         name: `Neko Blog`,
         short_name: `NEKO`,
-        start_url: `/blog/`,
+        start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#2d2b31`,
         display: `standalone`,
