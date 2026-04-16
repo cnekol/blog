69a17df326ea4a73828421fde2a19eab169458c5
cnekol <conan.neko.lin@gmail.com>
2026-04-17 00:40:00 +0800

    fix: realign theme with upstream typography

--- uno.config.js
+++ uno.config.js
@@ -47,6 +47,11 @@ export default defineConfig({
       /^col-(\d+)-(\d)$/,
       ([, start, end]) => ({ 'grid-column': `${start}/${end}` }),
     ],
+    [
+      /^scrollbar-hide$/,
+      ([_]) => `.scrollbar-hide { scrollbar-width:none;-ms-overflow-style: none; }
+      .scrollbar-hide::-webkit-scrollbar {display:none;}`,
+    ],
   ],
   presets: [
     presetWind3(),
